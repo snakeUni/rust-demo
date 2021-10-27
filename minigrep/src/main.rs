5dd3fd98824468baaca1aff9ed7b49c1dfc36f05
Uni <1025687605@qq.com>
2021-10-27 10:05:13 +0800

    feat: add minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+use colored::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    println!("{} {} !", "it".green(), "works".blue().bold());
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
