a5b425454468e87df1408b3768ecb97e9afb97da
Uni <1025687605@qq.com>
2021-11-03 10:10:19 +0800

    add

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,9 +5,9 @@ use minigrep::Config;
 use colored::*;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
