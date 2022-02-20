f667786aee6c3e14e633e3c1128f625bf2c591b9
Uni <1025687605@qq.com>
2022-02-20 11:18:21 +0800

    add

---
+++ iterating_over_result/src/main.rs
@@ -0,0 +1,12 @@
+// https://doc.rust-lang.org/rust-by-example/error/iter_result.html
+fn main() {
+    let strings = vec!["tofu", "93", "18"];
+    let (numbers, errors): (Vec<_>, Vec<_>) = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .partition(Result::is_ok);
+    let numbers: Vec<_> = numbers.into_iter().map(Result::unwrap).collect();
+    let errors: Vec<_> = errors.into_iter().map(Result::unwrap_err).collect();
+    println!("Numbers: {:?}", numbers);
+    println!("Errors: {:?}", errors);
+}
